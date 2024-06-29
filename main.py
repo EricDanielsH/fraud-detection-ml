cd510569c5b4f78139bc293677ed839e0fb68416
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-06-29 18:11:39 +0100

    Explore the data, handle missing values and do feature engineering

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 # Load the data to a DataFrame
 df = pd.read_csv("creditcard.csv")
+
+# Print basic information about the data to understand it
+print(df.info())
+print("====================================================")
+print(df.describe())
+print("====================================================")
+print(df.head())
+
+# Check for any missing values in any field
+print(df.isnull().sum())
+
+# Drop rows missing values if there are any
+print(df.dropna())
+
+# Normalise numericals fields
+numerical_fields = df.select_dtypes(include=['int64', 'float64']).columns
+# Create a scaler object
+scaler = StandardScaler()
+
+# Scale numerical fields
+df[numerical_fields] = scaler.fit_transform(df[numerical_fields])
+
+# See the difference with the original dataset
+# print(df)
+# print(df[numerical_fields])
+
