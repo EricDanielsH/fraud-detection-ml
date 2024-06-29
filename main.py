6f83bac3d5140fbf219b1f028a8652fb08ce0d49
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-06-30 00:42:31 +0100

    Remove hyperparameter tunning

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ df = pd.read_csv("creditcard.csv")
 print(df.isnull().sum())
 
 # Drop rows missing values if there are any
-print(df.dropna())
+df = df.dropna()
 
 # Separate Features from the Target
 features = df.drop(columns=['Class'])
@@ -51,36 +51,19 @@ gb_pipeline = Pipeline(steps=[('scaler', StandardScaler()), ('classifier', Gradi
 
 pipelines = [lr_pipeline, rf_pipeline, gb_pipeline]
 
-# Train the models
+# Train the models and optimise
 for pipeline in pipelines:
     pipeline.fit(features_train, target_train)
-
-# Evaluate the model
-for pipeline in pipelines:
+    
+    # Evaluate the best models with the test set
     # Predict the class labels for the testing training set
-    target_pred = pipeline.predict(features_train)
+    target_pred = pipeline.predict(features_test)
     # Predict the probability estimates for the positive class(1). ROC AUC
-    target_prob = pipeline.predict_proba(features_train)[:, 1]
+    target_prob = pipeline.predict_proba(features_test)[:, 1]
 
-    # Print clasification report
-    print("Clasification report: ")
-    print(classification_report(target_train, target_pred))
+    print(f"Classification report for {pipeline.steps[-1][1].__class__.__name__}:")
+    print(classification_report(target_test, target_pred))
     
-    # Calculate the ROC AUC score
-    print("ROC-AUC score: ")
-    print("ROC-AUC: ", roc_auc_score(target_train, target_prob), "\n")
+    print(f"ROC-AUC score for {pipeline.steps[-1][1].__class__.__name__}: {roc_auc_score(target_test, target_prob)}\n")
 
     
-# Optimise the model:
-
-# Example: Grid Search for Random Forest
-param_grid = {
-    'classifier__n_estimators': [100, 200, 300],
-    'classifier__max_depth': [10, 20, 30]
-}
-
-grid_search = GridSearchCV(rf_pipeline, param_grid, cv=5, scoring='roc_auc')
-grid_search.fit(features_train, target_train)
-
-print("Best parameters:", grid_search.best_params_)
-print("Best score:", grid_search.best_score_)
