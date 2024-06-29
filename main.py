5e308f01cf01799be1ae4a42212ba9b8c6e86092
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-06-29 18:21:03 +0100

    Separete features from targets

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 
 # Load the data to a DataFrame
 df = pd.read_csv("creditcard.csv")
@@ -19,13 +20,17 @@ print(df.isnull().sum())
 # Drop rows missing values if there are any
 print(df.dropna())
 
+# Separate Features from the Target
+features = df.drop(columns=['Class'])
+target = df['Class']
+
 # Normalise numericals fields
-numerical_fields = df.select_dtypes(include=['int64', 'float64']).columns
+numerical_fields = features.select_dtypes(include=['int64', 'float64']).columns
 # Create a scaler object
 scaler = StandardScaler()
 
 # Scale numerical fields
-df[numerical_fields] = scaler.fit_transform(df[numerical_fields])
+features = scaler.fit_transform(features)
 
 # See the difference with the original dataset
 # print(df)
